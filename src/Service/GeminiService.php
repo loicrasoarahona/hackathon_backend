8d2a3922c0b4fad2175552bde43852c1f1273797
loicrasoarahona <88525546+loicrasoarahona@users.noreply.github.com>
2025-11-14 17:27:53 +0300

    Update GeminiService.php

--- src/Service/GeminiService.php
+++ src/Service/GeminiService.php
@@ -50,6 +50,8 @@ class GeminiService
 
         $responseArray = json_decode($response, true);
 
+        dd($responseArray);
+
         // 3. Analyse de la Réponse
         if (isset($responseArray['candidates'][0]['content']['parts'][0]['text'])) {
             $jsonText = trim($responseArray['candidates'][0]['content']['parts'][0]['text']);
